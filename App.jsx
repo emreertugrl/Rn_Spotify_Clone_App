55ccb5f507587dd77754912d024d26836b5b4ddb
emreertugrl <emrertugrl@gmail.com>
2025-04-20 13:42:43 +0300

    stack and bottom tab navigation

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import Navigation from './src/navigation/Routes';
+import {SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>App 1</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <Navigation />
+    </SafeAreaView>
   );
 }
---
+++ src/navigation/Routes.js
@@ -0,0 +1,38 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import LikedSongScreen from '../screens/LikedSongScreen';
+import SongInfoScreen from '../screens/SongInfoScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        {/* <Stack.Screen name="Main" component={BottomTabs} /> */}
+        <Stack.Screen name="Liked" component={LikedSongScreen} />
+        <Stack.Screen name="Info" component={SongInfoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
