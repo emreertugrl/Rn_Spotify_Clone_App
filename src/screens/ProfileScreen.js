9070b7823bf4560defb4ecfb58a1835474b30393
emreertugrl <emrertugrl@gmail.com>
2025-04-21 20:46:05 +0300

    profile screen completed

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,23 @@
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
 import LinearGradient from 'react-native-linear-gradient';
+import {ProfileContext} from '../context/ProfileContext';
+import round from 'lodash/round';
 
 const ProfileScreen = () => {
+  const {profileData, loading, error} = useContext(ProfileContext);
+
+  const {name, image_url, followers_count, public_playlists} = profileData;
+
+  const formatFollowers = count => {
+    if (count >= 1000000) {
+      //* ikinic parametre işlemin kaç ondalık basamağa göre yapılacağını belirler
+      return `${round(count / 1000000, 1)}M`;
+    }
+    if (count >= 1000) {
+      return `${round(count / 1000, 1)}K`;
+    }
+  };
   return (
     <LinearGradient colors={['#040306', '#131624']} style={{flex: 1}}>
       <ScrollView
@@ -10,34 +25,38 @@ const ProfileScreen = () => {
         contentContainerStyle={{paddingBottom: 100}}>
         <View style={{padding: 12}}>
           <View style={styles.profileContainer}>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              style={styles.profileImage}
-            />
+            <Image source={{uri: image_url}} style={styles.profileImage} />
             <View>
-              <Text style={styles.profileName}>Emre</Text>
-              <Text style={styles.profileFollowers}>5 followers</Text>
+              <Text style={styles.profileName}>{name}</Text>
+              <Text style={styles.profileFollowers}>
+                {formatFollowers(followers_count)} followers
+              </Text>
             </View>
           </View>
         </View>
         <Text style={styles.sectionTitle}>Your Playlists</Text>
         <View style={styles.playlistContainer}>
-          <View
-            style={{
-              marginVertical: 10,
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 8,
-            }}>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              style={styles.playlistImage}
-            />
-            <View>
-              <Text style={styles.playlistName}>Duman</Text>
-              <Text style={styles.playlistFollowers}>50 followers</Text>
+          {public_playlists.map(playlist => (
+            <View
+              key={playlist.uri}
+              style={{
+                marginVertical: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
+                gap: 8,
+              }}>
+              <Image
+                source={{uri: 'https://picsum.photos/200/300'}}
+                style={styles.playlistImage}
+              />
+              <View>
+                <Text style={styles.playlistName}>{playlist.name}</Text>
+                <Text style={styles.playlistFollowers}>
+                  {formatFollowers(playlist.followers_count)} followers
+                </Text>
+              </View>
             </View>
-          </View>
+          ))}
         </View>
       </ScrollView>
     </LinearGradient>
