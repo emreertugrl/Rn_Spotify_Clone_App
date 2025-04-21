6aad25121411b64a1cda15592058afd382cb7ecb
emreertugrl <emrertugrl@gmail.com>
2025-04-21 19:51:56 +0300

    song ınfo screen design completed

--- src/screens/SongInfoScreen.js
+++ src/screens/SongInfoScreen.js
@@ -16,113 +16,50 @@ import {
   Play,
   ShieldCross,
 } from 'iconsax-react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
 const SongInfoScreen = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+  const {album} = route?.params || {};
+  const {artist, coverArt, name, year} = album;
   return (
     <LinearGradient colors={['#040306', '#131624']} style={{flex: 1}}>
-      <ScrollView style={{marginTop: 20}}>
-        <View style={{padding: 12}}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.paddingView}>
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <ArrowLeft color="white" size={24} />
           </TouchableOpacity>
-          <View style={{flex: 1, alignItems: 'center'}}>
-            <Image
-              source={{uri: 'https://picsum.photos/200/300'}}
-              style={{width: 200, height: 200, borderRadius: 5}}
-            />
+          <View style={styles.imageView}>
+            <Image source={{uri: coverArt}} style={styles.coverImage} />
           </View>
-          <Text
-            style={{
-              color: 'white',
-              marginHorizontal: 12,
-              marginTop: 10,
-              fontSize: 22,
-              fontWeight: 'bold',
-            }}>
-            Name
-          </Text>
-          <View
-            style={{
-              marginHorizontal: 12,
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              gap: 7,
-              marginTop: 10,
-            }}>
-            <Text style={{color: '#909090', fontSize: 13, fontWeight: 'bold'}}>
-              name
-            </Text>
+          <Text style={styles.albumNameText}>{name}</Text>
+          <View style={styles.artistView}>
+            <Text style={styles.artistText}>{artist}</Text>
           </View>
         </View>
 
-        <Pressable
-          style={{
-            marginHorizontal: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <Pressable
-            style={{
-              backgroundColor: '#1db954',
-              width: 30,
-              height: 30,
-              borderRadius: 15,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+        <Pressable style={styles.controlView}>
+          <Pressable style={styles.downloadButton}>
             <ArrowDown size={24} color="white" />
           </Pressable>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 10,
-            }}>
+          <View style={styles.playButtonView}>
             <ShieldCross size="24" color="#1db954" />
-            <Pressable
-              style={{
-                backgroundColor: '#1db954',
-                justifyContent: 'center',
-                alignItems: 'center',
-                width: 60,
-                height: 60,
-                borderRadius: 30,
-              }}>
+            <Pressable style={styles.playButton}>
               <Play size="24" color="white" variant="Bold" />
             </Pressable>
           </View>
         </Pressable>
 
         <View>
-          <View style={{marginHorizontal: 12, marginTop: 10}}>
-            <Pressable
-              style={{
-                marginVertical: 10,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
-              <View>
-                <Text style={{color: 'white', fontWeight: '500', fontSize: 16}}>
-                  name
-                </Text>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    marginTop: 5,
-                    gap: 8,
-                  }}>
-                  <Text
-                    style={{color: 'white', fontWeight: '500', fontSize: 16}}>
-                    artist
-                  </Text>
-                </View>
-              </View>
-              <More size={30} color="white" />
-            </Pressable>
+          <View style={styles.infoView}>
+            <View>
+              <Text style={styles.infoText}>Album: {name}</Text>
+              <Text style={styles.infoText}>Artist: {artist}</Text>
+              <Text style={styles.infoText}>Year: {year}</Text>
+            </View>
+
+            <More size={30} color="white" />
           </View>
         </View>
       </ScrollView>
@@ -132,4 +69,62 @@ const SongInfoScreen = () => {
 
 export default SongInfoScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollView: {marginTop: 50},
+  paddingView: {padding: 10},
+  imageView: {flex: 1, alignItems: 'center'},
+  coverImage: {
+    width: 200,
+    height: 200,
+  },
+  albumNameText: {
+    color: 'white',
+    marginHorizontal: 12,
+    marginTop: 10,
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  artistView: {
+    marginHorizontal: 12,
+    marginTop: 10,
+  },
+  artistText: {
+    color: '#909090',
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  controlView: {
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  downloadButton: {
+    backgroundColor: '#1DB954',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playButtonView: {flexDirection: 'row', gap: 10, alignItems: 'center'},
+  playButton: {
+    backgroundColor: '#1DB954',
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+  },
+  infoView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 12,
+    marginTop: 10,
+  },
+  infoText: {color: 'white', fontWeight: '500', fontSize: 16},
+  infoContainer: {
+    gap: 5,
+  },
+});
